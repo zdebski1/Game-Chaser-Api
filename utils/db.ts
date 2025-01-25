dbb3a4d04d5421e40428b696b8ec0fa229f9735b
zdebs <zdebski1@gmail.com>
2025-01-25 11:38:36 -0500

    adding mysql

---
+++ utils/db.ts
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'Admin',
+  password: 'Abc123',
+  database: 'GameChaser',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool;
