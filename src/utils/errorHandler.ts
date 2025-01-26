bc4b4782dd922652f054f3041df6587d7ee663c5
zdebs <zdebski1@gmail.com>
2025-01-26 10:18:58 -0500

    removing files

--- src/utils/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { ServerResponse } from 'http';
-
-export const handleError = (res: ServerResponse, error: Error) => {
-  console.error('Error:', error.message);
-
-  res.writeHead(400, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ success: false, message: error.message }));
-};
